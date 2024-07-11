c75965d2238b75561143a1526bd833309cabda31
szilyx <forgacsszilard1@gmail.com>
2024-07-11 20:51:43 +0300

    Complete server and database connecting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(()=>{
+    async function fetchArtists(){
+      try{
+        const resp = await fetch("http://localhost:3000/artists");
+        if(!resp.ok){
+          throw new Error('Network responese was not ok');
+        }
+        const data = await resp.json();
+        setArtists(data);
+      }catch(error){
+        console.error("Error fetchin data:", error);
+      }
+    }
+    fetchArtists();
+  },[]);
+
     return (
     <div className="container">
-      <h1>Szia</h1>
+      <h1>Előadók</h1>
+      <ul>
+        {artists.map(artist => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
